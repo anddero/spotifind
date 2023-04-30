7535b8a551db4d841d61e9500fc1c4752ee0f4b1
Karl-Andero Mere <anddero@gmail.com>
2023-04-30 18:32:42 +0300

    Implement song liking functionality.

--- script.js
+++ script.js
@@ -39,6 +39,15 @@ function findAll(arr, predicate) {
     return indexes;
 }
 
+function segmentArray(arr, segLen) {
+    if (segLen <= 0) throw 'Segment length must be higher than 0';
+    const newArr = [];
+    for (let i = 0; i < arr.length; i += segLen) {
+        newArr.push(arr.slice(i, i + segLen));
+    }
+    return newArr;
+}
+
 function wait(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
@@ -187,6 +196,22 @@ function spotifyFind(prompt, token) {
         });
 }
 
+function spotifyLike(songIds, token) {
+    if (songIds.length > 20) throw 'Can like only up to 20 songs at a time';
+    const url = `https://api.spotify.com/v1/me/tracks?ids=${songIds.join(',')}`;
+    return fetch(url, {
+        method: 'PUT',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        }
+    }).then(response => ({
+            status: 'success',
+            response: response.status,
+            songIds: songIds.join(',')
+        }));
+}
+
 async function spotifyFindNoThrow(prompt, token) {
     try {
         return await spotifyFind(prompt, token);
@@ -199,8 +224,79 @@ async function spotifyFindNoThrow(prompt, token) {
     }
 }
 
+async function spotifyLikeNoThrow(songIds, token) {
+    try {
+        return await spotifyLike(songIds, token);
+    } catch (error) {
+        return {
+            status: 'error',
+            response: error,
+            songIds: songIds.join(',')
+        };
+    }
+}
+
 async function spotifyFindAll(promptArr, token) {
     return Promise.all(await mapWithInterval(promptArr, (prompt) => spotifyFindNoThrow(prompt, token), 100));
+}
+
+async function spotifyLikeAll(songIds, token) {
+    const segments = segmentArray(songIds, 20);
+    return Promise.all(await mapWithInterval(segments, (seg) => spotifyLikeNoThrow(seg, token), 300));
+}
+
+async function redirectToAuthCodeFlow(clientId) {
+    const verifier = generateCodeVerifier(128);
+    const challenge = await generateCodeChallenge(verifier);
+
+    localStorage.setItem("verifier", verifier);
+
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("response_type", "code");
+    params.append("redirect_uri", "http://localhost:63342/spotifind/");
+    params.append("scope", "user-library-modify");
+    params.append("code_challenge_method", "S256");
+    params.append("code_challenge", challenge);
+
+    document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+}
+
+function generateCodeVerifier(length) {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+async function generateCodeChallenge(codeVerifier) {
+    const data = new TextEncoder().encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+}
+
+function getSensitiveAccessToken(clientId, code) {
+    const verifier = localStorage.getItem("verifier");
+
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("grant_type", "authorization_code");
+    params.append("code", code);
+    params.append("redirect_uri", "http://localhost:63342/spotifind/");
+    params.append("code_verifier", verifier);
+
+    return fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: params.toString()
+    }).then(parseFetchAsJson)
+        .then(response => response.access_token);
 }
 
 // UI
@@ -235,7 +331,18 @@ function downloadAll(promptArr, respArea) {
             downloadStringAsFile(onlyIds.join('\n'), 'IDs.txt');
             const onlyNames = cleanArr.map((el) => el.fullStr);
             downloadStringAsFile(onlyNames.join('\n'), 'Names.txt');
-        }).catch(logAndDisplayError);
+        }).catch(error => logAndDisplayError(error, respArea));
+}
+
+function likeAll(songIds, respArea) {
+    const tokenInput = document.getElementById('token-input');
+    spotifyLikeAll(songIds, tokenInput.value)
+        .then(response => {
+            logAndDisplayResponse(response, respArea);
+            return response;
+        }).then(respArr => {
+            downloadStringAsFile(JSON.stringify(respArr, null, 4), 'LikeResults.json');
+        }).catch(error => logAndDisplayError(error, respArea));
 }
 
 function bindUiToken(respArea) {
@@ -245,7 +352,7 @@ function bindUiToken(respArea) {
         const secretInput = document.getElementById('secret-input');
         spotifyGenToken(clientIdInput.value, secretInput.value)
             .then(response => logAndDisplayResponse(response, respArea))
-            .catch(logAndDisplayError);
+            .catch(error => logAndDisplayError(error, respArea));
     };
 }
 
@@ -256,7 +363,7 @@ function bindUiSearch(respArea) {
         const findInput = document.getElementById('find-input');
         spotifyFind(findInput.value, tokenInput.value)
             .then(response => logAndDisplayResponse(response, respArea))
-            .catch(logAndDisplayError);
+            .catch(error => logAndDisplayError(error, respArea));
     };
 }
 
@@ -275,8 +382,61 @@ function bindUiFindAll(respArea) {
     };
 }
 
+function bindUiLike(respArea) {
+    const likeButton = document.getElementById('like-button');
+    likeButton.onclick = () => {
+        const tokenInput = document.getElementById('token-input');
+        const songIdInput = document.getElementById('song-id-input');
+        spotifyLike(songIdInput.value.split(','), tokenInput.value)
+            .then(response => logAndDisplayResponse(response, respArea))
+            .catch(error => logAndDisplayError(error, respArea));
+    };
+}
+
+function bindUiLikeAll(respArea) {
+    const fileInput = document.getElementById('like-file-input');
+    fileInput.onchange = () => {
+        const file = fileInput.files[0];
+        if (!file) throw 'no file selected';
+        const reader = new FileReader();
+        reader.onload = function(event) {
+            const contents = event.target.result;
+            const lines = contents.split(/\r?\n/);
+            likeAll(lines, respArea);
+        };
+        reader.readAsText(file);
+    };
+}
+
+function bindUiAuth() {
+    const authButton = document.getElementById('auth-button');
+    authButton.onclick = () => {
+        const clientIdInput = document.getElementById('client-id-input');
+        redirectToAuthCodeFlow(clientIdInput.value);
+    };
+}
+
+function generateTokenIfAuth(respArea) {
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get("code");
+
+    if (code) {
+        console.log('code', code);
+        const clientIdInput = document.getElementById('client-id-input');
+        getSensitiveAccessToken(clientIdInput.value, code)
+            .then(token => logAndDisplayResponse(token, respArea))
+            .catch(error => logAndDisplayError(error, respArea));
+    } else {
+        console.log('no code');
+    }
+}
+
 const respArea = document.getElementById('response-container');
 respArea.innerText = "no response yet";
 bindUiToken(respArea);
 bindUiSearch(respArea);
 bindUiFindAll(respArea);
+bindUiLike(respArea);
+bindUiLikeAll(respArea);
+bindUiAuth();
+generateTokenIfAuth(respArea);
